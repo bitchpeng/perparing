9ec48b0bf50c6a15ea5a55448597bda427bd7ac5
superpeng <bitchpeng@163.com>
2022-02-12 01:21:35 +0800

    init

---
+++ java/java-demo/src/main/java/com/bitchpeng/javademo/JavaDemoApplication.java
@@ -0,0 +1,17 @@
+package com.bitchpeng.javademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class JavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoApplication.class, args);
+
+
+    }
+
+}
